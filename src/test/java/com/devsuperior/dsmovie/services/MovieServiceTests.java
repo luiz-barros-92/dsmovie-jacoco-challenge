ff1aa4c8776d1e5c4bfa8a90d6162e194736351e
Luiz Barros <lck.barros@gmail.com>
2026-02-02 22:46:50 -0300

    test: MovieService findByid

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,9 +1,29 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
@@ -11,8 +31,32 @@ public class MovieServiceTests {
 	@InjectMocks
 	private MovieService service;
 	
+	@Mock
+	private MovieRepository repository;
+	
+	private String title;
+    private PageImpl<MovieEntity> page;
+    private MovieEntity movie;
+	
+    @BeforeEach
+    void setUp() throws Exception {
+    	title = "The Witcher";
+    	movie = new MovieEntity(1L, title, 4.5, 2, "www.imagefromthisfilm.com");
+    	page = new PageImpl<>(List.of(movie));
+    	
+    	when(repository.searchByTitle(ArgumentMatchers.anyString(), (Pageable) ArgumentMatchers.any()))
+        .thenReturn(page);
+    }
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<MovieDTO> result = service.findAll(title, pageable);
+		
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+		assertEquals(result.getContent().get(0).getTitle(), title);
+		
+		verify(repository, times(1)).searchByTitle(title, pageable);
 	}
 	
 	@Test
